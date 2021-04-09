63468afb2e39e57e2ddc7c5f7fb407cf36d12ce0
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-04-09 16:42:55 +0300

    Add files via upload

--- Lab_3.cs
+++ Lab_3.cs
@@ -9,11 +9,11 @@ namespace Lab_3
         private double _defaultHealth;
         private double _inFightHealth;
         private double _defaultDamage;
-        private double _inFightDamage   ;
-        private string _colour = "Green";
+        private double _inFightDamage;
         private string _name = "Creature " + _totalAmount;
         private static int _totalAmount = 1;
         //TO DO
+        private string _colour = "Green";
         //private static int _hybridsAmount;
 
         public MagicalСreature(int age, double health, double damage)
@@ -92,24 +92,23 @@ namespace Lab_3
             }
         }
 
-        public void Attack(MagicalСreature isAttacked)
+        public int Attack(MagicalСreature isAttacked)
         {
             if (_name == isAttacked._name)
             {
-                Console.WriteLine($"{_name} is a little bit dump " +
+                Console.WriteLine($"\n{_name} is a little bit dump " +
                                   $"and tries to attack itself");
-                return;
+                return -1;
             }
 
-            Console.WriteLine($"{_name} attacks {isAttacked._name}");
+            Console.WriteLine($"\n{_name} attacks {isAttacked._name}\n");
 
             if (isAttacked._inFightHealth <= 0)
             {
                 Console.WriteLine(isAttacked._name +
                                   " is already defeated, no need to attack");
-                return;
+                return -1;
             }
-
 
             isAttacked._inFightHealth -= _defaultDamage;
             if (isAttacked._inFightHealth <= 0)
@@ -117,6 +116,7 @@ namespace Lab_3
                 isAttacked._inFightHealth = 0;
                 Console.WriteLine(isAttacked._name + " is defeated.");
             }
+            return 1;
         }
 
         public void MakeSound(int temp)
@@ -140,11 +140,12 @@ namespace Lab_3
             }
         }
 
-        public void PrintInFightInfo()
+        public int PrintInFightInfo()
         {
-            Console.WriteLine($"{_name} infight characteristics");
+            Console.WriteLine($"\n{_name} current characteristics");
             Console.WriteLine($"HP:\t {_inFightHealth}/{_defaultHealth}");
             Console.WriteLine($"Damage:\t {_inFightDamage}/{_defaultDamage}\n");
+            return -1;
         }
 
         public void Revive()
@@ -153,30 +154,110 @@ namespace Lab_3
             _inFightDamage = _defaultDamage;
         }
 
-        public void Healing()
+        public int Healing()
         {
             if (_inFightDamage == 1)
             {
-                Console.WriteLine($"{_name} attack point is 1, " +
+                Console.WriteLine($"\n{_name} attack point is 1, " +
                                   $"there is no opportunite to heal");
-                return;
+                return -1;
             }
 
-            Console.WriteLine($"{_name} sacrifices 1 attack point " +
+            Console.WriteLine($"\n{_name} sacrifices 1 attack point " +
                               $"to get 10 HP ");
             --_inFightDamage;
             _inFightHealth += 10;
+            return 1;
         }
-    } 
+    }
 
     class Lab_3
     {
+        public static void FighterChoise(MagicalСreature activeFighter,
+                                  MagicalСreature passiveFighter)
+        {
+            Console.WriteLine($"{activeFighter[4]} is acting");
+            Console.WriteLine("Make chooise\n" +
+                              "1) Attack\n2) Heal\n3) Print fight info");
+            int turn = -1;
+            uint choise;
+            while (turn != 1)
+            {
+                choise = GetValidUint();
+
+                switch (choise)
+                {
+                    case 1:
+                        turn = activeFighter.Attack(passiveFighter);
+                        break;
+                    case 2:
+                        turn = activeFighter.Healing();
+                        break;
+                    case 3:
+                        turn = activeFighter.PrintInFightInfo();
+                        break;
+                    default:
+                        Console.WriteLine("\nThere is no such an opportunity");
+                        break;
+                }
+            }
+        }
+
+        public static int Fight(MagicalСreature firstFighter,
+                          MagicalСreature secondFighter)
+        {
+            byte turn = 1;
+            while (firstFighter.InFightHealth > 0 &&
+                   secondFighter.InFightHealth > 0)
+            {
+                if (turn == 1)
+                {
+                    FighterChoise(firstFighter, secondFighter);
+                    turn = 2;
+                }
+                else 
+                {
+                    FighterChoise(secondFighter, firstFighter);
+                    turn = 1;
+                }
+            }
+
+            if (turn == 2)
+            {
+                Console.WriteLine($"{firstFighter[4]} wins!");
+                turn = 1;
+            }
+            else
+            {
+                Console.WriteLine($"{secondFighter[4]} wins!");
+                turn = 2;
+            }
+            return turn;
+        }
+
+        static uint GetValidUint()
+        {
+            uint tempUint;
+            bool inputCheck = uint.TryParse(Console.ReadLine(), out tempUint);
+
+            while (!inputCheck)
+            {
+                Console.WriteLine("Oops, wrong input, try again");
+                inputCheck = uint.TryParse(Console.ReadLine(), out tempUint);
+            }
+            return tempUint;
+        }
+
+        public static void Revive(MagicalСreature deadMonster)
+        {
+            Console.WriteLine($"\nSomething weired is happend: {deadMonster[4]} revived\n");
+            deadMonster.Revive();
+        }
+
         static void Main(string[] args)
         {
-
             Random temp = new Random();
             MagicalСreature monster = new MagicalСreature(123, 90, 10);
-
             monster.MakeSound(temp.Next(0,4));
             monster.PrintInFightInfo();
 
@@ -185,12 +266,12 @@ namespace Lab_3
             notMonster.MakeSound(temp.Next(0, 4));
             notMonster.PrintInFightInfo();
 
-            monster.Attack(notMonster);
-            notMonster.PrintInFightInfo();
-            notMonster.Attack(monster);
-            monster.PrintInFightInfo();
+            int winner = Fight(monster, notMonster);
 
- 
+            if (winner == 1) Revive(notMonster);
+            else Revive(monster);
+
+            Fight(notMonster, monster);
         }
     }
 }
