7d23f13b1d789369a8c18cfd8ac74794e868a71a
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-05-07 18:24:55 +0300

    Add files via upload

--- Lab5,6/Class1.cs
+++ Lab5,6/Class1.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Class1
+
+    public interface IMyComparable<in T>
     {
+       int MyCompareTo(T other);
+    }
+
+    static void Sort<T>(T[]array) where T : IComparable<T>
+    {
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < array.Length; ++i)
+        {
+            for (i = 0; i < array.Length; ++i)
+            { 
+                if (array[i] > array[j])
+                {
+                    T temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    class Class1 : IMyComparable<T>
+
+    {
+
+        public int CompareTo(T toCompare)
+        {
+            if (toCompare == null) return 1;
+
+            if (toCompare < this) return -1;
+            if (toCompare == this) return 0;
+            return 1;
+        }
     }
 }
